a965ac861804eba6f68f994149470e60c3e22718
ullas <ullaskaruvatumana@gmail.com>
2025-08-27 23:26:38 +0530

    made routes aboutus and contactus

--- routes/web.php
+++ routes/web.php
@@ -2,40 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', function(){
-    return "hello from laravel";
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/about', function(){
-    return "about page";
+
+Route::get('/about-us', function(){
+    $name = "John Doe";
+    $age = 25;
+    return view('aboutus')->with('name', $name)->with('age', $age);;
 });
 
-// Route::get('details/students', function(){
-//     return "students details page";
-// });
-
-// Route::get('details/teachers', function(){
-//     return "Teachers details page";
-// });
-
-Route::prefix('details')->group(function(){
-    Route::get('students', function(){
-        return "students details page";
-    })->name('students-details');
-
-    Route::get('teachers', function(){
-        return "Teachers details page";
-    })->name('teachers-details');
-});
-
-Route::get("user/{id}/{reg}", function($id,$reg){
-    return "user id is :" . $id . " registration number is :" . $reg;
-});
-
-Route::fallback(function(){
-    return "404 page not found";
-});
+// Route::view("about-us", "aboutus");
